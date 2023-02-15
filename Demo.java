5de73ccc1632156baeabc126a1117ff6a7276ae6
Jarvood11 <36446076+Jarvood11@users.noreply.github.com>
2023-02-16 01:25:04 +0300

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+
+    public static String greet() {
+        return "Hello";
+    }
+    public static void main(String[] args) {
+        System.out.println(greet());
+
+    }
+
+}
